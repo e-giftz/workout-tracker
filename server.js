3aff3bf5f6cd6c5b4127980bc82fb6a72f304c89
Gift <56089461+e-giftz@users.noreply.github.com>
2021-07-07 02:44:19 -0400

    updating the server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const router = require("./routes/api.js");
+//const router = require("./routes/api.js");
 
 const PORT = process.env.PORT || 3000
 
@@ -14,13 +14,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true, 
+  useCreateIndex: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
+//app.use(require("./routes/html.js"));
+require("./routes/html.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
