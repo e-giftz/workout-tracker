777f09efe5438dcc34450b41b63e89f02b9cbcc1
Gift <56089461+e-giftz@users.noreply.github.com>
2021-07-07 03:32:53 -0400

    Adding routes to the api.js file

--- routes/api.js
+++ routes/api.js
@@ -14,4 +14,52 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+// Routes to add an exercise
+router.put("/api/workouts/:id", ({ params, body}, res)  => {
+    console.log("PARAMS", body, params);
+
+    Workout.findOneAndUpdate(
+        {
+            _id: params.id
+        },
+        {
+            $push: {
+                exercises: body
+            }
+        },
+        {
+            new: true
+        }
+    )
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    });
+});
+
+// Routes to get all workouts within a range
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+    .limit(7)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
+
+// Routes to get the last workout
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
+
 module.exports = router;
